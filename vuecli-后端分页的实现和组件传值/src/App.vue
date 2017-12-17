1100fe76c44d66f233540df8432102c17d3433f2
yuanxin <1017123610@qq.com>
2017-12-17 19:24:11 +0800

    Add files via upload

---
+++ vuecli-后端分页的实现和组件传值/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+      <vfrom></vfrom>
+      <vtable></vtable>
+      <pagination></pagination>
+  </div>
+
+</template>
+
+<script>
+import vfrom from "./components/vfrom/vfrom"
+import vtable from "./components/vtable/vtable"
+import pagination from "./components/pagination/pagination"
+export default {
+ components:{
+   vfrom,
+   vtable,
+   pagination
+ }
+}
+</script>
+
+<style>
+</style>
